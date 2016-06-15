7c8da2de2d1814c313388c05a89f78479551c947
Erik Hennerfors <erik.hennerfors@adeprimo.se>
2016-06-15 13:20:39 +0200

    Obscured.RRD added ruby version to gemfile

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,15 +2,16 @@
 # this point, so we have to point up a directory.
 app_path = File.expand_path(File.dirname(__FILE__) + '/..')
 
-timeout 300
 worker_processes 4
+timeout 300
+preload_app true
 working_directory app_path
 pid app_path + '/tmp/unicorn.pid'
 
 stderr_path app_path + '/logs/unicorn.stderr.log'
 stdout_path app_path + '/logs/unicorn.stdout.log'
 
-listen app_path + '/tmp/unicorn.sock', backlog: 64
+listen app_path + '/tmp/unicorn.sock', :backlog  => 2048
 
 before_exec do |server|
   ENV['BUNDLE_GEMFILE'] = "#{app_path}/Gemfile"
